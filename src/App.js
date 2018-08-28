8cfb764c9234546ccd9aecd4c2e7c5ca095e45e1
Luiz Cunha <luiz.cunha.3@gmail.com>
2018-08-27 22:57:05 -0300

    CRA

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="landing"></div>
+                <div className="content">Ola</div>
+            </div>
+        );
+    }
+}
+
+export default App;
